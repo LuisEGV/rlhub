56f480adf0778576a2af7db638bca34048e7b078
Luis Gonzalez <luis@kadi.club>
2021-04-23 16:28:54 -0700

    Final Upload Before Submission

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -56,18 +56,13 @@ function Landing() {
                 Get started
               </a>
             </div>
-            <div className="mt-3 sm:mt-0 sm:ml-3">
-              <p className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
-                View Models
-              </p>
-            </div>
           </div>
         </main>
       </div>
       <div class="container mx-auto">
         <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-2xl md:text-3xl">
           <span className="block text-indigo-600 xl:inline p-2">
-            Browse Some Examples
+            Browse Some Environments
           </span>
         </h1>
         <div class="flex flex-wrap">
